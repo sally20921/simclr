8b3114cc745e4cb6225909b06edcf25f3f1c93f4
sally20921 <sally20921@snu.ac.kr>
2020-05-24 15:34:28 +0900

    modules change

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 '''
 sh setup.sh
 conda activate simclr
+pip3 install -r requirements.txt
 '''
 
 '''
--- modules/projection_head.py
+++ modules/projection_head.py
@@ -6,5 +6,11 @@ to the  space  where  contrastive  loss is applied.
 
 2-layer MLP projection head  
 
-to obtain z_i = W*ReLU(W*h_i) 
+to obtain z_i = W*ReLU(W*h_i)
+
+
+self.projector = nn.Sequential(
+    nn.Linear()
+    nn.ReLU()
+    nn.Linear()
 '''
