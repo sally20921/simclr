6d1a7251b44599f58ed2631af299dc46c1e980f3
sally20921 <sally20921@snu.ac.kr>
2020-05-24 15:39:19 +0900

    need modification  on model.py  and  main.py

--- main.py
+++ main.py
@@ -6,4 +6,7 @@ import  argparse
 
 from modules import NT_Xent
 from modules import Augmentation
+from model import load_model, save_model
+
+def train(args,  train_loader, model, criterion, optimizer, writer):
 
--- model.py
+++ model.py
@@ -1,3 +1,10 @@
 import  os 
 import torch  
 from modules import SimCLR, LARS
+
+def load_model(args, loader):
+    model=SimCLR(args)
+    model=model.to(args.device)
+
+    learning_rate  =  0.3 * args.batch_size /256
+
