1563befded067edb169f9a8776fbb913a5fb1e06
sally20921 <sally20921@snu.ac.kr>
2020-05-24 20:42:28 +0900

    main.py

--- main.py
+++ main.py
@@ -8,5 +8,36 @@ from modules import NT_Xent
 from modules import Augmentation
 from model import load_model, save_model
 
-def train(args,  train_loader, model, criterion, optimizer, writer):
+def train(args,  train_loader, model, criterion, optimizer):
+    loss_epoch = 0
+    
+    return loss_epoch
+
+def main(_run, _log):
+    args = argparse.Namespace(**_run.config)
+    args.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    root = "./datasets"
+
+    train_dataset = torchvision.datasets.CIFAR10(
+            root, download=True, transform=Augmentation(size=32)
+        )
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=args.batch_size,
+        shuffle=(train_sampler is None),
+        drop_last=True,
+        num_workers=args.workers,
+        sampler=train_sampler,
+    
+    )
+
+    model, optimizer = load_model(args, train_loader)
+    
+    criterion = NT_Xent(args.batch_size, args.temperature, args.device)
+
+    save_model(args, model, optimizer)
+
+
 
--- model.py
+++ model.py
@@ -7,4 +7,16 @@ def load_model(args, loader):
     model=model.to(args.device)
 
     learning_rate  =  0.3 * args.batch_size /256
+     optimizer = LARS(
+            model.parameters(),
+            lr=learning_rate,
+            weight_decay=args.weight_decay,
+            exclude_from_weight_decay=["batch_normalization", "bias"],
+        )
+
+    return model, optimizer
+
+def save_model(args, model, optimizer):
+    out  = os.path.join(args.out_dir, "checkpoint_{}.tar".format(args.current_epoch))
+    torch.save(model.state_dict(), out)
 
