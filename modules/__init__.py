40f75422f4c13ba82ad478d4a816af1fd257cb7c
sally20921 <sally20921@github.com>
2020-06-06 16:14:35 +0900

    erasing

--- modules/__init__.py
+++
@@ -1,4 +0,0 @@
-from .simclr  import SimCLR
-from .loss_function import NT_Xent
-from .lars import LARS
-from .data_augmentation import Augmentation
--- modules/base_encoder.py
+++
@@ -1,9 +0,0 @@
-'''
-base encoder f 
-that extracts representation vectors 
-from augmented data examples. 
-
-We adopt the commonly used ResNet-50
-
-to obtain h_i (the output  after the average  pooling layer) 
-'''
